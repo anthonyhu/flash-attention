d2395679fcef5c535e86ed5db0f3a94cd080593b
Anthony Hu <anthony@wayve.ai>
2023-04-27 09:55:34 +0100

    add pyproject.toml file

--- setup.py
+++ setup.py
@@ -181,6 +181,8 @@ setup(
     cmdclass={"build_ext": BuildExtension} if ext_modules else {},
     python_requires=">=3.7",
     install_requires=[
+        "torch",
         "einops",
+        "packaging",
     ],
 )
